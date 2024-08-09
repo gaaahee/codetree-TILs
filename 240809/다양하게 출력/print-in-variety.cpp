f03e23c4fd8fa5d5e5ae699b89a134d99d2cb853
gaaahee <163043607+gaaahee@users.noreply.github.com>
2024-08-09 20:43:40 +0900

    Title: 다양하게 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240809/다양하게 출력/print-in-variety.cpp
+++ 240809/다양하게 출력/print-in-variety.cpp
@@ -6,6 +6,6 @@ int main() {
     cout << "Total days in Year" << endl;
     cout << 365 << endl;
     cout << "Circumference rate" << endl;
-    cout << num;
+    cout << fixed << precision(12) << 3.1415926535;
     return 0;
 }
