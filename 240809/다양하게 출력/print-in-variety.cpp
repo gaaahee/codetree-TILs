2d9b3b74152ca25f6197dbba7f7c992a98d8520f
gaaahee <163043607+gaaahee@users.noreply.github.com>
2024-08-09 20:48:56 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240809/다양하게 출력/print-in-variety.cpp
+++ 240809/다양하게 출력/print-in-variety.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-    double num = 3.1415926535;
     cout << "Total days in Year" << endl;
     cout << 365 << endl;
     cout << "Circumference rate" << endl;
     cout << fixed;
-    cout.precision(12);
-    cout << num;
+    cout.precision(10);
+    cout << 3.1415926535;
     return 0;
 }
