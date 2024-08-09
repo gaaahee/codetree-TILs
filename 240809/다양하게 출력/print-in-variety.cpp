302564da51f278ace770c579a4fe0d85a12ee162
gaaahee <163043607+gaaahee@users.noreply.github.com>
2024-08-09 20:42:37 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/다양하게 출력/print-in-variety.cpp
+++ 240809/다양하게 출력/print-in-variety.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main() {
+    double num = 3.1415926535;
     cout << "Total days in Year" << endl;
     cout << 365 << endl;
     cout << "Circumference rate" << endl;
-    cout << 3.1415926535;
+    cout << num;
     return 0;
 }
